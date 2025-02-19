b65e92ee08daa8c1030f5c6ff5f4b87ef1c9daae
Eric Barroca <eric@composable.sh>
2025-02-20 00:04:37 +0900

    feat: add MeterNames enum to define various meter types

--- packages/common/src/meters.ts
+++ packages/common/src/meters.ts
@@ -13,3 +13,12 @@ export interface MeterAdjustment {
 export interface AdjustMetersMeterWorkflowParams {
     adjustments: MeterAdjustment[];
 }
+
+export enum MeterNames {
+    analyzed_pages = 'analyzed_pages',
+    extracted_tables = 'extracted_tables',
+    analyzed_images = 'analyzed_images',
+    input_token_used = 'input_token_used',
+    output_token_used = 'output_token_used',
+    task_run = 'task_run',
+}
