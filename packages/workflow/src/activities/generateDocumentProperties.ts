3287ab034bd05d8d7b28578d5d58d2d4acfac216
Leon Ruggiero <lruggiero@becomposable.com>
2025-04-04 11:18:36 +0100

    improve getText() logic

--- packages/workflow/src/activities/generateDocumentProperties.ts
+++ packages/workflow/src/activities/generateDocumentProperties.ts
@@ -1,8 +1,10 @@
 import { log } from "@temporalio/activity";
-import { DSLActivityExecutionPayload, DSLActivitySpec } from "@vertesia/common";
+import { CreateContentObjectPayload, DSLActivityExecutionPayload, DSLActivitySpec } from "@vertesia/common";
 import { setupActivity } from "../dsl/setup/ActivityContext.js";
 import { TruncateSpec } from "../utils/tokens.js";
 import { InteractionExecutionParams, executeInteractionFromActivity } from "./executeInteraction.js";
+import { ContentObject } from "@vertesia/common";
+import { Create } from "sharp";
 
 const INT_EXTRACT_INFORMATION = "sys:ExtractInformation";
 export interface GenerateDocumentPropertiesParams extends InteractionExecutionParams {
@@ -77,22 +79,29 @@ export async function generateDocumentProperties(
         promptData,
         payload.debug_mode ?? false,
     );
+
     const getText = () => {
-        if (doc.text) {
+        // If there is already text, and it is not trivially improveable by adding a description,
+        // return undefined to keep the original text
+        if (doc.text
+            && doc.text !== infoRes.result.title
+            && doc.text !== doc.name) {
             return undefined;
         }
+
         let text = "";
         if (infoRes.result.title) {
-            text += infoRes.result.title + "\n";
+            text += infoRes.result.title;
         }
         if (infoRes.result.description) {
+            if (text) {
+                text += "\n";
+            }
             text += infoRes.result.description;
         }
-        if (text) {
-            return text;
-        } else {
-            return undefined;
-        }
+
+        // If falsy (e.g. empty string), return file name
+        return text ? text : doc.name;
     };
 
     log.info(`Extracted information from object ${objectId} with type ${type.name}`, { runId: infoRes.id });
