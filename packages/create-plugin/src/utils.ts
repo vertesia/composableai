ca56533791868310e9d2899c00d9e218f9e8a2a3
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2025-05-07 17:38:08 +0200

    v0.55.0

--- packages/create-plugin/src/utils.ts
+++ packages/create-plugin/src/utils.ts
@@ -1,7 +1,6 @@
 import { spawnSync } from "node:child_process";
 
 export function runCommand(cmd: string, args: string[]) {
-    console.log('>>>>>>>>>>>>>', cmd, args.join(' '));
     spawnSync(cmd, args, {
         stdio: 'inherit'
     });
