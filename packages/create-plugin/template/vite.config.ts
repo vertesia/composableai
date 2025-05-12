486b8f52c4436be768d93020db17ad0e486e7c61
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2025-05-12 18:36:04 +0200

    added shared ui to externals

--- packages/create-plugin/template/vite.config.ts
+++ packages/create-plugin/template/vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts'; // Generates TypeScript declarations
 import { exportPluginCss } from '@vertesia/vite-plugin-export-css';
+import { version } from "./package.json"
 
 const EXTERNALS = [
   'react',
   'react-dom',
   'react/jsx-runtime',
   'react-dom/client',
+  '@vertesia/ui',
   // add any other external dependencies here
 ];
 
@@ -32,7 +34,7 @@ export default defineConfig(({ command }) => {
         entry: './src/index.tsx', // Main entry point of your library
         formats: ['es'], // Build ESM versions
         name: '${plugin_var_name}',
-        fileName: "${plugin_name}",
+        fileName: `${plugin_name}-${version}`,
       } : undefined,
       minify: false,
       rollupOptions: {
