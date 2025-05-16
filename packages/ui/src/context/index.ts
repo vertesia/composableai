a19fdae160918d8f28ef3ada2ebbff612090c46a
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2025-05-16 16:10:37 +0200

    remove context dispatch

--- packages/ui/src/context/index.ts
+++ packages/ui/src/context/index.ts
@@ -1,21 +1,72 @@
-/**
- * This file contains the host context which is a set of live components and hooks shared
- * with the plugins.
- * The plugin must import a component or hook from the host context by using:
- * import {SomeComponnet, useSomeHook } from '@vetesia/ui-extension-sdk/context';
- */
+import type { VertesiaClient, ZenoClient } from "@vertesia/client";
+import type { AccountRef, AuthTokenPayload, ProjectRef } from "@vertesia/common";
 
-import { HOST_CONTEXT_VAR } from "./HostContext.js";
-import type { HostContext } from "./HostContext.js";
+export const HOST_CONTEXT_VAR = '__vetesia_host_context__';
 
-const context = (globalThis as any)[HOST_CONTEXT_VAR] as HostContext;
+// TODO This file redeclare some types from the composable UI / router
+// we need to make these pblics and move shared composable-ui parts here.
 
-export const {
-    useUserSession,
-    useRouterContext,
-    useNavigate,
-    useLocation,
-    NestedRouterProvider,
-} = context;
+export interface UserSession {
+    isLoading: boolean;
+    client: VertesiaClient;
+    authError?: Error;
+    authToken?: AuthTokenPayload;
+    lastSelectedAccount?: string | null;
+    lastSelectedProject?: string | null;
+    onboardingComplete?: boolean;
+    store: ZenoClient;
+    user?: AuthTokenPayload;
+    account?: AccountRef;
+    accounts?: AccountRef[];
+    project?: ProjectRef;
+    rawAuthToken: Promise<string>
+}
+export interface NavigateOptions {
+    replace?: boolean;
+    state?: any;
+    /**
+     * if defined prepend the basePath to the `to` argument
+     */
+    basePath?: string;
+}
 
-export * from "./HostContext.js";
+export interface RouterContext {
+    location: Location,
+    // route: Route,
+    // router: BaseRouter,
+    params: Record<string, string>,
+    state: any,
+    /**
+     * The path that matched the route. For widlcard `/*` paths this doens;t include the wildcard part.
+     * You can get the wildcard path from `remainingPath`.
+     */
+    matchedRoutePath: string,
+    remainingPath?: string,
+    navigate: (path: string, options?: NavigateOptions) => void;
+}
+type LazyImportFn = () => Promise<any>;
+interface ComponentRoute {
+    path: string;
+    Component: React.ComponentType<any>;
+}
+interface LazyComponentRoute {
+    path: string;
+    LazyComponent: LazyImportFn;
+}
+type Route = ComponentRoute | LazyComponentRoute;
+export interface NestedRouterProviderProps {
+    routes: Route[],
+    /**
+     * The path to use for the root resource. Defaults to '/'. Cannot contains path vairables or wildcards
+     */
+    index?: string;
+    children?: React.ReactNode;
+    fixLinks?: boolean;
+}
+export interface HostContext {
+    useUserSession: () => UserSession;
+    useRouterContext: () => RouterContext;
+    useNavigate: () => (path: string, options?: NavigateOptions) => void;
+    useLocation: () => Location;
+    NestedRouterProvider: React.ComponentType<NestedRouterProviderProps>;
+}
