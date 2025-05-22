225ada6f82f1e8acea75c79e14b14330b4b02090
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2025-05-22 19:43:19 +0200

    Added consistency check of external dependencies

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -5,6 +5,7 @@ import fs from 'fs';
 import path from 'path';
 import { defineConfig } from 'rollup';
 import { terser } from 'rollup-plugin-terser';
+import { EXTERNALS } from './externals.js';
 
 const outputDir = path.resolve('lib');
 const esmOutputDir = path.join(outputDir, 'esm');
@@ -31,51 +32,7 @@ const jsEntries = entries.map((name) => ({
         format: 'es',
         sourcemap: true,
     },
-    external: [
-        'react',
-        'react-dom',
-        'react/jsx-runtime',
-        'firebase',
-        'firebase/app',
-        'firebase/auth',
-        'firebase/analytics',
-        'jwt-decode',
-        "@headlessui/react",
-        "lucide-react",
-        "clsx",
-        "@radix-ui/react-checkbox",
-        "@radix-ui/react-dialog",
-        "@radix-ui/react-label",
-        "@radix-ui/react-popover",
-        "@radix-ui/react-separator",
-        "@radix-ui/react-slot",
-        "@radix-ui/react-tabs",
-        "@radix-ui/react-tooltip",
-        "class-variance-authority",
-        "cmdk",
-        "date-fns",
-        "lodash-es",
-        "motion",
-        /^motion\/.*/,
-        "react-day-picker",
-        "tailwind-merge",
-        "debounce",
-        "fast-xml-parser",
-        // codemirror
-        "codemirror",
-        "@codemirror/state",
-        "@codemirror/view",
-        "@codemirror/lang-json",
-        // verteisa deps
-        "@llumiverse/common",
-        "@vertesia/client",
-        "@vertesia/common",
-        "@vertesia/json",
-        "ajv",
-        ///^ajv\/.*/,
-
-        /^@vertesia\/ui\/.*/
-    ],
+    external: EXTERNALS,
     plugins: [
         nodeResolve({
             browser: true,
@@ -83,7 +40,7 @@ const jsEntries = entries.map((name) => ({
         }),
         json(),
         commonjs(),
-        //terser(),
+        terser(),
     ],
 }));
 
