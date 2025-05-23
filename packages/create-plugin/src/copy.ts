c02ccdfdce12b1dc9197501f5c594641c28409e2
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2025-05-23 15:54:34 +0200

    create-plugin tool for web plugins (#229)

--- packages/create-plugin/src/copy.ts
+++ packages/create-plugin/src/copy.ts
@@ -1,13 +1,18 @@
 import { promises as fs } from "fs";
 import { join } from "path";
+import { processVarsInFile } from "./template.js";
 
 /**
  * Recursively copies the contents of a source directory to a target directory using the most optimized method.
  *
  * @param sourceDir - The path to the source directory.
  * @param targetDir - The path to the target directory.
+ * @param templateSuffix - Optional suffix to remvoe from file name after copying. Ex: .template, .tmpl
  */
-export async function copyTree(sourceDir: string, targetDir: string): Promise<void> {
+export async function copyTree(sourceDir: string, targetDir: string, template?: {
+    suffix?: string, // Suffix to remove from file name after copying
+    context: Record<string, any> // Context for template processing
+}): Promise<void> {
     // Optimize by using a queue to avoid deep recursion and potential stack overflows
     const queue: { source: string; target: string }[] = [{ source: sourceDir, target: targetDir }];
 
@@ -21,8 +26,12 @@ export async function copyTree(sourceDir: string, targetDir: string): Promise<vo
         const items = await fs.readdir(source, { withFileTypes: true });
 
         for (const item of items) {
+            const isTemplate = template?.suffix ? item.name.endsWith(template.suffix) : false;
+            const targetName = isTemplate ? item.name.slice(0, -template!.suffix!.length) : item.name;
             const sourcePath = join(source, item.name);
-            const targetPath = join(target, item.name);
+            const targetPath = join(target, targetName);
+
+            console.log('Generating', targetPath);
 
             if (item.isDirectory()) {
                 // Queue subdirectory for processing
@@ -30,6 +39,10 @@ export async function copyTree(sourceDir: string, targetDir: string): Promise<vo
             } else if (item.isFile() || item.isSymbolicLink()) {
                 // Copy file or symbolic link
                 await fs.copyFile(sourcePath, targetPath);
+                if (isTemplate) {
+                    // Process template if applicable
+                    processVarsInFile(targetPath, template!.context);
+                }
             }
         }
     }
