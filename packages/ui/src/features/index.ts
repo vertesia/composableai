f055b483b42c6f8bdbfda2244f69da8a7a455289
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2025-06-02 21:37:10 +0200

    moved magic-pdf

--- packages/ui/src/features/index.ts
+++ packages/ui/src/features/index.ts
@@ -1,6 +1,7 @@
 export * from "./activity-doc/index.js";
 export * from "./errors/index.js";
 export * from "./layout/index.js";
+export * from "./magic-pdf/index.js";
 export * from "./permissions/index.js";
 export * from "./store/index.js";
 export * from "./user/index.js";
