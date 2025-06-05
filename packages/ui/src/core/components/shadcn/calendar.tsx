f7e55bc5bcba78462303a516433c7d1b92d3cc67
yangchi-tw <ykuo@vertesiahq.com>
2025-06-05 09:44:27 +0200

    Updated react-day-picker from 8.10.1 to 9.7.0 in the UI package

--- packages/ui/src/core/components/shadcn/calendar.tsx
+++ packages/ui/src/core/components/shadcn/calendar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker, StyledComponent } from "react-day-picker"
+import { DayPicker } from "react-day-picker"
 
 import { cn } from "../libs/utils"
 import { buttonVariants } from "./button"
@@ -71,12 +71,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }: StyledComponent) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }: StyledComponent) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, ...props }) => {
+          if (orientation === 'left') {
+            return <ChevronLeft className="h-4 w-4" {...props} />
+          }
+          return <ChevronRight className="h-4 w-4" {...props} />
+        },
       }}
       {...props}
     />
