2dcf060709f20d34567b87aee36db3a13948b31d
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2025-06-09 11:29:20 +0200

    added cors rules

--- packages/create-plugin/templates/tool/src/server.ts
+++ packages/create-plugin/templates/tool/src/server.ts
@@ -1,8 +1,9 @@
 import { Hono } from 'hono';
 import { registry } from "./index.js";
+import { cors } from 'hono/cors'
 
 const app = new Hono();
-
+app.get("/", cors({ origin: '*', allowMethods: ['GET'] }))
 app.post('/', async (c) => {
     const data = await c.req.json();
     const r = await registry.execute(data)
