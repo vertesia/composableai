95ee9986c4fca59452d83fcda3faff9fece2d657
Leon Ruggiero <lruggiero@becomposable.com>
2025-06-10 08:39:33 +0100

    fix: interaction model, clear on environment change or use default model (#295)

--- packages/ui/src/features/agent/PayloadBuilder.tsx
+++ packages/ui/src/features/agent/PayloadBuilder.tsx
@@ -5,6 +5,7 @@ import { useUserSession } from "@vertesia/ui/session";
 import Ajv, { ValidateFunction } from "ajv";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import type { JSONSchema4 } from "json-schema";
+import { supportsToolUse } from "@llumiverse/common";
 
 export interface ConversationWorkflowPayload {
     interaction?: Interaction | undefined;
@@ -67,17 +68,18 @@ export class PayloadBuilder implements ConversationWorkflowPayload {
             // Reset the validator when schema changes
             this._inputValidator = undefined;
             if (interaction) {
-                if (interaction.model) {
-                    this.payload.model = interaction.model;
-                } else {
-                    this.payload.model = undefined;
-                }
                 if (interaction.environment) {
                     if (typeof interaction.environment === 'string') {
                         this.vertesia.environments.retrieve(interaction.environment).then((environment) => this.environment = environment);
                     } else {
                         this.payload.environment = interaction.environment;
                     }
+                }
+                if (interaction.model) {
+                    this.payload.model = interaction.model;
+                } else {
+                    this.payload.model = this.environment?.default_model && supportsToolUse(this.environment.default_model, this.environment.provider)
+                        ? this.environment.default_model : undefined;
                 }
             }
             this.onStateChanged();
@@ -90,6 +92,9 @@ export class PayloadBuilder implements ConversationWorkflowPayload {
     set environment(environment: ExecutionEnvironmentRef | undefined) {
         if (environment?.id !== this.payload.environment?.id) {
             this.payload.environment = environment;
+            this.payload.model = environment?.default_model && supportsToolUse(environment.default_model, environment.provider)
+                ? environment.default_model : undefined;
+
             this.onStateChanged();
         }
     }
