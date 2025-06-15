c09511363d366fa87539a46674080f8e4b87ab41
yangchi-tw <ykuo@vertesiahq.com>
2025-06-15 16:58:17 +0200

    add stict mode info to content objects

--- packages/ui/src/widgets/properties/PropertiesView.tsx
+++ packages/ui/src/widgets/properties/PropertiesView.tsx
@@ -1,4 +1,5 @@
-import { Table } from "@vertesia/ui/core";
+import { Table, VTooltip } from "@vertesia/ui/core";
+import { Info } from "lucide-react";
 
 
 interface PropertiesViewProps {
@@ -12,8 +13,15 @@ export function PropertiesView({ className, properties }: PropertiesViewProps) {
                 {
                     properties.map((property) => (
                         <tr key={property.name}>
-                            <td className='w-1/3 font-semibold' >
+                            <td className='w-1/3 font-semibold gap-2' >
                                 <span>{property.name}</span>
+                                {property.description &&
+                                    <VTooltip
+                                        description={property.description}
+                                        placement="top">
+                                        <Info className="size-3 ml-2 text-muted-foreground" />
+                                    </VTooltip>
+                                }
                             </td>
                             <td className='w-2/3'>{property.value ?? 'undefined'}</td>
                         </tr>
