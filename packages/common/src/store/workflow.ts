9d772684c19efb7968d35aa87873799429c62b33
yangchi-tw <ykuo@vertesiahq.com>
2025-06-17 16:22:55 +0200

    specific interaction name in the interface

--- packages/common/src/store/workflow.ts
+++ packages/common/src/store/workflow.ts
@@ -247,7 +247,7 @@ export interface WorkflowRun {
     run_id?: string;
     workflow_id?: string;
     initiated_by?: string;
-    interaction?: string;
+    interaction_name?: string;
     input?: any;
     result?: any;
     error?:any,
