df52b023028b6cfe29ac84ff8d59486befa832bf
Yangchi KUO <164338465+yangchi-tw@users.noreply.github.com>
2025-07-21 15:09:24 +0200

    Feat: Invitation accept parameter (#433)

--- packages/client/src/AccountApi.ts
+++ packages/client/src/AccountApi.ts
@@ -48,13 +48,14 @@ export default class AccountApi extends ApiTopic {
     listInvites(): Promise<TransientToken<UserInviteTokenData>[]> {
         return this.get('/invites');
     }
-
     /**
-     * Fetch Invites for Account
+     * Fetch Invites for specific account or project
+     * @param type Type of the invitation, either "project" or "account"
+     * @param id ID of the project or organization to fetch invitations for
      * @returns UserInviteTokenData[]
      * */
-    listAccountInvitation(id: string): Promise<TransientToken<UserInviteTokenData>[]> {
-        return this.get(`/invites/${id}`);
+    listInvitation(id: string, type: "project" | "account" = "project"): Promise<TransientToken<UserInviteTokenData>[]> {
+        return this.get(`/invites/${type}/${id}`);
     }
 
     /**
