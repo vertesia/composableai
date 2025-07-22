8d918c1722f8559ad72fa33a1f2d5670e5fbff96
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2025-07-22 21:04:36 +0200

    Start conversation workflow using an agent token (#439)

--- packages/ui/src/shell/VertesiaShell.tsx
+++ packages/ui/src/shell/VertesiaShell.tsx
@@ -3,19 +3,21 @@ import { UserPermissionProvider } from "@vertesia/ui/features";
 import { UserSessionProvider } from "@vertesia/ui/session";
 import { SplashScreen } from "./SplashScreen";
 import { SigninScreen } from "./login/SigninScreen";
+import { ReactNode } from "react";
 
 
 interface VertesiaShellProps {
     children: React.ReactNode;
     lightLogo?: string;
     darkLogo?: string;
+    loadingIcon?: ReactNode;
 }
-export function VertesiaShell({ children, lightLogo, darkLogo }: VertesiaShellProps) {
+export function VertesiaShell({ children, lightLogo, darkLogo, loadingIcon }: VertesiaShellProps) {
     return (
         <ToastProvider>
             <UserSessionProvider>
                 <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-                    <SplashScreen />
+                    <SplashScreen icon={loadingIcon} />
                     <SigninScreen allowedPrefix="/shared/" lightLogo={lightLogo} darkLogo={darkLogo} />
                     <UserPermissionProvider>
                         {children}
