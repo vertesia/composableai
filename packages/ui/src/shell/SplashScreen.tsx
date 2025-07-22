fb5f08b3411a3d22fc12112c71d8d07c2a06da46
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2025-07-22 20:52:12 +0200

    customize splash screen

--- packages/ui/src/shell/SplashScreen.tsx
+++ packages/ui/src/shell/SplashScreen.tsx
@@ -1,11 +1,12 @@
 import { Transition } from "@headlessui/react";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, ReactNode, useEffect, useState } from "react";
 import { useUserSession } from "@vertesia/ui/session";
 
 
 interface SplashScreenProps {
+    icon?: ReactNode;
 }
-export function SplashScreen({ }: SplashScreenProps) {
+export function SplashScreen({ icon: Icon }: SplashScreenProps) {
     const { isLoading } = useUserSession();
     const [show, setShow] = useState(true);
 
@@ -32,10 +33,42 @@ export function SplashScreen({ }: SplashScreenProps) {
             <div style={{ zIndex: 999999 }} className='fixed inset-x-0 inset-y-0'>
                 <div className="flex w-full h-full items-center justify-center">
                     <div className="animate-[spin_4s_linear_infinite]">
-                        <img src='/icon.svg' className='w-10 h-auto animate-pulse rounded-full' />
+                        <div className='animate-pulse rounded-full bg-transparent'>
+                            {Icon || <LoadingIcon />}
+                        </div>
                     </div>
                 </div>
             </div>
         </Transition>
+    )
+}
+
+function LoadingIcon() {
+    const stopColor1 = "currentColor";
+    const stopColor2 = "currentColor";
+    // const stopColor1 = "#4F46E5";
+    // const stopColor2 = "#4F46E5";
+    return (
+        <svg
+            className="w-8 h-8 text-indigo-600"
+            viewBox="0 0 50 50"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <defs>
+                <linearGradient id="spinner-gradient" x1="1" y1="0" x2="0" y2="1">
+                    <stop offset="0%" stopColor={stopColor1} stopOpacity="1" />
+                    <stop offset="100%" stopColor={stopColor2} stopOpacity="0" />
+                </linearGradient>
+            </defs>
+            <circle
+                cx="25"
+                cy="25"
+                r="20"
+                stroke="url(#spinner-gradient)"
+                strokeWidth="5"
+                fill="none"
+                strokeLinecap="round"
+            />
+        </svg>
     )
 }
