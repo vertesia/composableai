0176dce9c2c0b8f09411cbe7751894814469e8bb
Mincong Huang <mhuang@vertesiahq.com>
2025-09-01 15:27:23 +0200

    Revert "feat: add workflow rate limiter activity (#512)"
    This reverts commit fc2c0a64066f150de5a3331b24de57bf8be443b9.

--- packages/workflow/src/activities/index-dsl.ts
+++ packages/workflow/src/activities/index-dsl.ts
@@ -18,4 +18,3 @@ export { transcribeMedia } from "./media/transcribeMediaWithGladia.js";
 export { notifyWebhook } from "./notifyWebhook.js";
 export { setDocumentStatus } from "./setDocumentStatus.js";
 export { identifyTextSections } from "./identifyTextSections.js";
-export { checkRateLimit } from "./rateLimiter.js";
