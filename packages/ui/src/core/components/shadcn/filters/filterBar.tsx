0964dc04b4d15880cf7964e5be7fead11a508dad
yangchi-tw <ykuo@vertesiahq.com>
2025-10-17 14:06:02 +0200

    add default setup for serach interface

--- packages/ui/src/core/components/shadcn/filters/filterBar.tsx
+++ packages/ui/src/core/components/shadcn/filters/filterBar.tsx
@@ -350,7 +350,7 @@ const FilterBar = ({ className }: { className?: string }) => {
   );
 };
 
-const FilterClear = ({ className }: { className?: string }) => {
+const FilterClear = ({ className, defaultValue }: { className?: string, defaultValue?: Filter[] }) => {
   const { filters, setFilters } = React.useContext(FilterContext);
 
   const hasActiveFilters = filters.filter((filter) => filter.value?.length > 0).length > 0;
@@ -364,7 +364,7 @@ const FilterClear = ({ className }: { className?: string }) => {
       variant="outline"
       size="xs"
       className={cn("transition group", className)}
-      onClick={() => setFilters([])}
+      onClick={() => setFilters(defaultValue || [])}
     >
       Clear All
     </Button>
