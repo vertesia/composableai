37381419d7fcfce31eb61a26d3ba43aba8e02cd1
Leon Ruggiero <lruggiero@vertesiahq.com>
2025-12-12 11:28:41 +0000

    time series support

--- packages/client/src/AnalyticsApi.ts
+++ packages/client/src/AnalyticsApi.ts
@@ -1,5 +1,5 @@
-import { RunAnalyticsQuery, RunAnalyticsResult, RunsAnalyticsSummary } from "@vertesia/common";
 import { ApiTopic, ClientBase } from "@vertesia/api-fetch-client";
+import { RunAnalyticsQuery, RunAnalyticsResult, RunsAnalyticsSummary } from "@vertesia/common";
 
 
 
@@ -13,10 +13,14 @@ export default class AnalyticsApi extends ApiTopic {
     runs(params: RunAnalyticsQuery): Promise<RunAnalyticsResult[]> {
         return this.post('/runs', { payload: params });
     }
-    
+
     //Unstable, likely to change - last update 1st Dec 2025
     runsSummary(): Promise<RunsAnalyticsSummary> {
         return this.get('/runs/summary');
+    }
+
+    runsTimeSeries(): Promise<Array<{ timestamp: Date; count: number }>> {
+        return this.get('/runs/time-series');
     }
 
 }
