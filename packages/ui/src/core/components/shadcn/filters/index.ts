0e4158d4cd4b444b81e9f5b1350fedcc1f055ba3
Leon Ruggiero <lruggiero@vertesiahq.com>
2026-01-14 20:08:47 +0000

    Remove filter-styles export from index.ts
    Removed export for filter-styles from index.ts

--- packages/ui/src/core/components/shadcn/filters/index.ts
+++ packages/ui/src/core/components/shadcn/filters/index.ts
@@ -1,6 +1,5 @@
 export * from './animateChangeInHeight';
 export * from './comboBox/comboBox';
-export * from './filter-styles';
 export * from './filterBar';
 export * from './filters';
 export * from './types';
