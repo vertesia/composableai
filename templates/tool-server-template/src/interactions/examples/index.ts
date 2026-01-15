9fcfb908c711a6c2730ac19ef0e16ca96e418a95
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2026-01-15 13:28:38 +0100

    cleanup template

--- templates/tool-server-template/src/interactions/examples/index.ts
+++ templates/tool-server-template/src/interactions/examples/index.ts
@@ -1,5 +1,5 @@
 import { InteractionCollection } from "@vertesia/tools-sdk";
-import textSummarizer from "./text_summarizer/index.js";
+import whatColor from "./what_color/index.js";
 import icon from "./icon.svg.js";
 
 export const ExampleInteractions = new InteractionCollection({
@@ -7,5 +7,5 @@ export const ExampleInteractions = new InteractionCollection({
     title: "Example Interactions",
     description: "A collection of interaction examples",
     icon,
-    interactions: [textSummarizer]
+    interactions: [whatColor]
 });
