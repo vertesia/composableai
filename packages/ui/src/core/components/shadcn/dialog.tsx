61cb6751e7aaacf3bc7f9c3dda46b26cc80ce2fb
Charles Morman <github@cmorman.com>
2026-01-25 15:35:31 -0700

    fix: add `usePortalContainer` to dialog component

--- packages/ui/src/core/components/shadcn/dialog.tsx
+++ packages/ui/src/core/components/shadcn/dialog.tsx
@@ -6,6 +6,7 @@ import { X } from "lucide-react";
 import { Button } from "./button";
 import { VisuallyHidden } from "../libs/visuallyHidden";
 import { createContext, useContext } from "react";
+import { usePortalContainer } from "../../hooks/PortalContainerProvider";
 
 interface ModalProps {
     children: React.ReactNode | React.ReactNode[];
@@ -205,25 +206,28 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 const DialogContent = React.forwardRef<
     React.ElementRef<typeof DialogPrimitive.Content>,
     React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
-    <DialogPortal>
-        <DialogOverlay />
-        <DialogPrimitive.Content
-            ref={ref}
-            autoFocus={false}
-            onOpenAutoFocus={(event) => {
-                event.preventDefault();
-            }}
-            className={cn(
-                "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] border bg-background shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
-                className
-            )}
-            {...props}
-        >
-            {children}
-        </DialogPrimitive.Content>
-    </DialogPortal>
-));
+>(({ className, children, ...props }, ref) => {
+    const container = usePortalContainer();
+    return (
+        <DialogPortal container={container}>
+            <DialogOverlay />
+            <DialogPrimitive.Content
+                ref={ref}
+                autoFocus={false}
+                onOpenAutoFocus={(event) => {
+                    event.preventDefault();
+                }}
+                className={cn(
+                    "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] border bg-background shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+                    className
+                )}
+                {...props}
+            >
+                {children}
+            </DialogPrimitive.Content>
+        </DialogPortal>
+    );
+});
 DialogContent.displayName = DialogPrimitive.Content.displayName;
 
 const DialogDescription = React.forwardRef<
