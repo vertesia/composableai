9351ce6b9982eac57a815ae7f8eeeb1d4ff932cd
Charles Morman <cmorman@vertesiahq.com>
2026-01-27 22:54:12 +0100

    fix: update UI components to use portal container (apps/shadow dom) (#823)

--- packages/ui/src/core/components/shadcn/popover.tsx
+++ packages/ui/src/core/components/shadcn/popover.tsx
@@ -4,6 +4,7 @@ import * as PopoverPrimitive from "@radix-ui/react-popover";
 import { cn } from "../libs/utils"
 import { JSX } from "react";
 import { useIsInModal } from "./dialog";
+import { usePortalContainer } from "../../hooks/PortalContainerProvider";
 
 export interface PopoverContextValue {
   open: boolean;
@@ -87,9 +88,10 @@ const PopoverContent = React.forwardRef<
   }
 
   const { setOpen, hover } = context;
+  const container = usePortalContainer();
 
   return (
-    <PopoverPrimitive.Portal>
+    <PopoverPrimitive.Portal container={container}>
       <PopoverPrimitive.Content
         ref={ref}
         align={align}
