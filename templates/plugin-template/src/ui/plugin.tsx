abdf46b0bbb66dd592803f3748f5473b49d63555
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2026-01-27 11:53:42 +0100

    Unified plugin template (#832)

---
+++ templates/plugin-template/src/ui/plugin.tsx
@@ -0,0 +1,18 @@
+import { PortalContainerProvider } from "@vertesia/ui/core";
+import { App } from "./app";
+
+/**
+ * Export the plugin component.
+ */
+export default function TEMPLATE__PluginComponentName({ slot }: { slot: string }) {
+    if (slot === "page") {
+        return (
+            <PortalContainerProvider>
+                <App />
+            </PortalContainerProvider>
+        );
+    } else {
+        console.warn('No component found for slot', slot);
+        return null;
+    }
+}
