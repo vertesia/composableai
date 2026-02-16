ebd4c75436e515ed0ee24b9962fe6d7c1f935307
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2026-02-16 15:14:56 +0100

    Feat plugin content types contrib (#915)

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -11,6 +11,7 @@ export * from "./PendingAsksApi.js";
 export * from "./QueryApi.js";
 export * from "./RenderingApi.js";
 export * from "./SchedulesApi.js";
+export * from "./TypeCatalogApi.js";
 export * from "./TypesApi.js";
 export * from "./WorkflowsApi.js";
 
