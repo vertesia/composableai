a081bcc080fae675929c64904b816c291d8a398a
yangchi-tw <ykuo@vertesiahq.com>
2026-02-23 17:27:05 +0100

    improve json rendering consistency

--- packages/ui/src/widgets/json-view/JSONDisplay.tsx
+++ packages/ui/src/widgets/json-view/JSONDisplay.tsx
@@ -17,8 +17,11 @@ export function JSONDisplay({ value, viewCode = false }: JSONDisplayProps) {
     }
 
     return (
-        <div className='relative w-full h-full'>
-            {viewCode ? <JSONCode data={value} /> : <JSONView value={value} />}
+        <div className='relative w-full h-full flex flex-col'>
+            {viewCode
+                ? <JSONCode data={value} />
+                : <div className="flex-1 min-h-0 overflow-auto"><JSONView value={value} /></div>
+            }
         </div>
     )
 }
