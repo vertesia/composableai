0d3582266505bf45970f4a33b6e0f18ca6b5362e
Charles Morman <github@cmorman.com>
2026-02-26 13:33:33 -0700

    feat: update slot to include "content" value

--- templates/plugin-template/src/ui/plugin.tsx
+++ templates/plugin-template/src/ui/plugin.tsx
@@ -5,7 +5,10 @@ import { App } from "./app";
  * Export the plugin component.
  */
 export default function TEMPLATE__PluginComponentName({ slot }: { slot: string }) {
-    if (slot === "page") {
+    // Render the plugin component based on the slot.
+    // Slot "page" is used in the App Portal
+    // Slot "content" is used in the Composite App
+    if (slot === "page" || slot === "content") {
         return (
             <PortalContainerProvider>
                 <App />
