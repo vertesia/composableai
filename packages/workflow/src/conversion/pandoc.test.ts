471f8fc24b1e65f05527ca5f1692144b68e988ac
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2026-02-26 18:39:21 +0900

    optimize and cleanup

--- packages/workflow/src/conversion/pandoc.test.ts
+++ packages/workflow/src/conversion/pandoc.test.ts
@@ -1,15 +1,12 @@
-import { MockActivityEnvironment, TestWorkflowEnvironment } from '@temporalio/testing';
+import { MockActivityEnvironment } from '@temporalio/testing';
 import fs from 'fs';
 import path from 'path';
 import { beforeAll, expect, test } from 'vitest';
 import { markdownWithPandoc } from '../conversion/pandoc';
 
-
-let testEnv: TestWorkflowEnvironment;
 let activityContext: MockActivityEnvironment;
 
 beforeAll(async () => {
-    testEnv = await TestWorkflowEnvironment.createLocal();
     activityContext = new MockActivityEnvironment();
 });
 
