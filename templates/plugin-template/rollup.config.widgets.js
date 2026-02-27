1e16c469417c89dbc72024cdf91dc5b01436d9b0
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2026-02-27 14:50:35 +0900

    chore: replace deprecated rollup-plugin-terser with @rollup/plugin-terser (#969)

--- templates/plugin-template/rollup.config.widgets.js
+++ templates/plugin-template/rollup.config.widgets.js
@@ -11,7 +11,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import { globSync } from 'fs';
 import path from 'path';
 
