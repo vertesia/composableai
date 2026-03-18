9ea1c2982832f13546c340c76c6b7222ae3cb56d
Leon Ruggiero <lruggiero@vertesiahq.com>
2026-03-18 16:55:46 +0000

    fix build, for force build

--- templates/plugin-template/vite.server.config.ts
+++ templates/plugin-template/vite.server.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import devServer from '@hono/vite-dev-server';
 import { vertesiaDevServerPlugin } from '@vertesia/build-tools/vite';
 
 export default defineConfig({
     plugins: [
         vertesiaDevServerPlugin(),
-        devServer({
-            entry: 'tools/server.ts'
-        }),
+        devServer({ entry: 'tools/server.ts' }) as PluginOption, //type assertion, helps when TS gets confused between 2 vite installs.
     ],
     build: {
         target: 'esnext',
