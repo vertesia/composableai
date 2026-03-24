c4fb25fbabb82f22177341bf87a0eafb1b769fa4
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2026-03-24 16:08:28 +0900

    feat: generic OAuth Application types, client (#1063)

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -11,6 +11,7 @@ export * from './environment.js';
 export * from "./facets.js";
 export * from './group.js';
 export * from './integrations.js';
+export * from './oauth.js';
 export * from './interaction.js';
 export * from './pending-asks.js';
 export * from './json-schema.js';
