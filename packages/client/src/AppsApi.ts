1fc1f06a99f7cbcf28a9dce3b3dd6885b7c720d7
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2026-04-17 15:14:29 +0900

    add scopes

--- packages/client/src/AppsApi.ts
+++ packages/client/src/AppsApi.ts
@@ -43,7 +43,7 @@ export default class AppsApi extends ApiTopic {
     install(
         appId: string,
         settings?: Record<string, any>,
-        oauthParams?: Record<string, { client_id?: string; client_secret?: string }>,
+        oauthParams?: Record<string, { client_id?: string; client_secret?: string; scopes?: string[] }>,
     ): Promise<AppInstallation> {
         return this.post(`/install`, {
             payload: {
