a89a469f5a2ef482ef0f892248115483e20a8631
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2026-05-08 10:16:17 +0200

    Revert "sync OAuth types from main (OAuthDeviceAuthorization, PrincipalType.OAuthAccess)"
    This reverts commit 06205d06e19d3901c51b8abf741afcecfe4c520c.

--- packages/common/src/apikey.ts
+++ packages/common/src/apikey.ts
@@ -138,7 +138,6 @@ export interface AuthTokenPayload {
 
 export enum PrincipalType {
     User = "user",
-    OAuthAccess = "oauth_access",
     Group = "group",
     ApiKey = "apikey",
     ServiceAccount = "service_account",
